61200375f9ea5bb4d27ed95c62ecf3076f7bb26e
vshen0520 <42666102+vshen0520@users.noreply.github.com>
2022-11-14 22:18:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from read_setting import read_setting
 from crack_game import main_worker
-from visualize_solution import visualize_solution
+from visualize_solution import visualizer
 
 if __name__=="__main__":
     # Select one board to play
-    f = './LazorTemplates/mad_1.bff'
+    f = './LazorTemplates/mad_4.bff'
 
     # Read the setting of the board
     block_grid, blocks, lazors, end_points = read_setting(f)
 
     # Crack the board
     location_key,lazor_path =  main_worker(block_grid, blocks, lazors, end_points)
-    
-    # Visualize the solution
-    # solution = visualizer_solution(np.shape(block_grid)[1], np.shape(block_grid)[0], 80)
+    # print(location_key)
+
+    solution = visualizer(block_grid, 80, location_key, lazor_path, end_points)
